2180da58469faa6537258a85b00353cf062ef93f
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-17 14:58:33 +0900

    Feat :: Header뷰 개발 #4

---
+++ baobao/src/components/Common/Header/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const HeaderWrap = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 8vh;
+    //background-color: antiquewhite;
+    display: flex;
+    align-items: center;
+`;
+
+export const Title = styled.span`
+    font-size: 50px;
+    color: #7AAA55;
+    font-family: Tenada, serif;
+    margin-left: 2vw;
+`;
+
+export const Select = styled.div`
+    font-family: Pretendard-ExtraBold, serif;
+    font-size: 22px;
+    color: #8C8C8C;
+    cursor: pointer;
+    display: flex;
+    gap: 10.0rem;
+    margin-left: 20vw;
+`;
