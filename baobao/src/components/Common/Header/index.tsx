22c78c7b3678a23b6f29c55ba0b0c7d20fb43d34
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-17 20:25:37 +0900

    Feat :: 이동경로 추가 #3

--- baobao/src/components/Common/Header/index.tsx
+++ baobao/src/components/Common/Header/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import * as S from "src/components/Common/Header/style";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const handleCampaign = () => {
+
+    }
     return (
         <S.HeaderWrap>
             <S.Title>바오바오</S.Title>
             <S.Select>
-                <span>캠페인</span>
+                <span onClick={() => {
+                    navigate("/campaign")
+                }}>캠페인</span>
                 <span>나무키우기</span>
                 <span>퀴즈</span>
                 <span>커뮤니티</span>
