a69b09a26f4f7df0dd677727a3333b7a8240df96
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-18 02:14:07 +0900

    Feat :: 나무링크 추가 및 디자인 수정 #10

--- baobao/src/components/Common/Header/index.tsx
+++ baobao/src/components/Common/Header/index.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import * as S from "src/components/Common/Header/style";
 import { useNavigate } from "react-router-dom";
+import "src/assets/scss/common.scss";
 
 const Header = () => {
     const navigate = useNavigate();
-    const handleCampaign = () => {
 
-    }
     return (
         <S.HeaderWrap>
             <S.Title>바오바오</S.Title>
             <S.Select>
-                <span onClick={() => {
-                    navigate("/campaign")
-                }}>캠페인</span>
-                <span>나무키우기</span>
+                <span onClick={() => navigate("/campaign")}>캠페인</span>
+                <span onClick={() => navigate("/tree")}>나무키우기</span>
                 <span>퀴즈</span>
                 <span>커뮤니티</span>
                 <span>프로필</span>
             </S.Select>
         </S.HeaderWrap>
     );
-}
+};
 
 export default Header;
--- baobao/src/components/Tree/index.tsx
+++ baobao/src/components/Tree/index.tsx
@@ -1,14 +1,15 @@
-import React, {Suspense, useRef} from "react";
-import {Canvas, useLoader} from "@react-three/fiber";
-import {OrbitControls, useGLTF} from '@react-three/drei'
+import React, { Suspense, useRef } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import "src/assets/scss/common.scss"
 
 const Tree = () => {
-    const {scene} = useGLTF('./model/tree/bigTree.glb');
+    const { scene } = useGLTF('./model/tree/bigTree.glb');
     return (
         <Canvas>
-            <ambientLight intensity={0.5}/>
-            <primitive object={scene}></primitive>
-            <OrbitControls/>
+            <ambientLight intensity={1} />
+            <primitive object={scene} position={[0, -3, 0]} scale={[0.4, 0.4, 0.4]} />
+            <OrbitControls />
         </Canvas>
     );
 };
